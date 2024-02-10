89ffc65ca059cbfb5cc0905b76b2a50c778093ab
anushkadusane <anushkaa147852@gmail.com>
2024-02-10 15:58:23 +0530

    test and list are being added in first function.

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ def CheckCurrentDate(self, dtvalue):
             currentdt = str(datetime.datetime.now()).split(" ")[0].split("-")
             crrdtobj = datetime.date(int(currentdt[0]), int(currentdt[1]), int(currentdt[2]))
             inputdt = str(dtvalue)
+            test = ''
             if ( inputdt.find("/") >= 0 ):
                 inputdtsplit = inputdt.split("/")
                 inputdtobj = datetime.date(int(inputdtsplit[2]), int(inputdtsplit[0]), int(inputdtsplit[1]))
+                list1 = []
             else:
                 inputdtobj = datetime.date(int(inputdt[:4]), int(inputdt[4:6]), int(inputdt[6:]))
             if ( crrdtobj < inputdtobj ):
@@ -24,6 +26,7 @@ def CheckDate(self, fromdt, todt):
             tomnth = int(todt[4:6])
             frmday = int(fromdt[6:])
             today = int(todt[6:])
+
             if frmyear > toyear:
                     return "To Date should be greater than From Date"
             elif (frmnth > tomnth and ( not (frmyear < toyear))):
